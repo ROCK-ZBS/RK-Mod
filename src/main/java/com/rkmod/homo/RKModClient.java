4230e8a132502d5da7c64f05824a52f6d926d2f0
steven <steven@macos-desktop-114514.local>
2023-06-26 21:51:03 +0800

    Initial commit

---
+++ src/main/java/com/rkmod/homo/RKModClient.java
@@ -0,0 +1,10 @@
+package com.rkmod.homo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RKModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/rkmod/homo/RKModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rkmod.homo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RKModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
